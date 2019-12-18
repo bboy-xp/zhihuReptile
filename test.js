48d5232cd54acbb5d9dc8f7504a94f7463045b37
xp <s_shixp@jiedaibao.com>
2019-12-18 11:58:20 +0800

    fix content and add videoArr

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const cheerio = require("cheerio");
-let url = "https://www.zhihu.com/question/352899305";
+let url = "https://www.zhihu.com/question/35931336/answer/228278520";
 let Data = {};
 axios.get(url).then(
     res => {
@@ -10,21 +10,30 @@ axios.get(url).then(
             decodeEntities: false
         });
         console.log("------------------");
-        let content = $('p, b, blockquote').text();
+        let content = $('.RichContent-inner').find('p, b, blockquote, strong').text();
         //图片
         let imgArr = [];
         $('figure').find('img').each(function (i, e) {
             let imgurl = $(this).attr("data-default-watermark-src");
-            // console.log(imgurl);
             if (imgurl) {
                 imgArr.push(imgurl);
             }
         });
+        // 视频
+        let videoArr = [];
+        $('.video-box').find('.url').each(function(i, e) {
+            let videoUrl = $(this).text();
+            if (videoUrl) {
+                videoArr.push(videoUrl);
+            }
+        });
         let data = {
             content: content,
-            imgArr: imgArr
+            imgArr: imgArr,
+            videoArr: videoArr
         };
         Data = data;
         console.log(Data);
+        console.log("------------------");
     }
 )
